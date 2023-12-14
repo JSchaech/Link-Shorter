6998ec40cdbbd39c45ab532a2e6c723a2f92b66c
Justin Schäch <99103163+JSchaech@users.noreply.github.com>
2023-12-14 15:55:38 +0100

    Delete URL_Shorter/static/script.js
    was not needed

--- URL_Shorter/static/script.js
+++
@@ -1,8 +0,0 @@
-function redirectToCustomURL() {
-    var customURL = document.getElementsByName('uC_URL')[0].value;
-    if (customURL.trim() !== "") {
-        window.location.href = "/INFO/" + customURL;
-        return false; // Prevent the form from submitting
-    }
-    return true; // Allow the form to submit if uC_URL is empty
-}
